3dceceeb529448f5058aa6aff95e67c8314429f0
Dominic König <dominic@nursix.org>
2016-09-29 14:50:12 +0200

    Start working on form builder

--- www/js/services/forms.js
+++ www/js/services/forms.js
@@ -27,12 +27,122 @@
 
 EdenMobile.factory('$emForm', [function () {
 
+    var createWidget = function(field, attr) {
+
+        var fieldType = field.type,
+            widgetType;
+
+        // @todo: ability to override the widget
+
+        switch(fieldType) {
+            case 'date':
+                widgetType = '<em-date-widget>';
+                break;
+            default:
+                widgetType = '<em-text-widget>';
+                break;
+        }
+
+        var widget = angular.element(widgetType);
+
+        if (attr) {
+            for (var a in attr) {
+                widget.attr(a, attr[a]);
+            }
+        }
+
+        return widget;
+    }
+
+
     /**
-     * The $emForms API
+     * Form API
+     */
+    function Form(schema, fields) {
+
+        var self = this;
+        self.schema = schema;
+        self.fields = fields;
+
+        self.render = function(scopeName) {
+
+            if (!scopeName) {
+                scopeName = 'form';
+            }
+
+            var form = angular.element('<div class="list">'),
+                fields = self.fields,
+                schema = self.schema;
+
+            for (var i=0, len=fields.length; i<len; i++) {
+
+                var fieldName = fields[i],
+                    fieldParameters = schema[fieldName],
+                    widget = null;
+
+                if (fieldParameters) {
+                    var fieldAttr = {
+                        'label': fieldParameters.label,
+                        'ng-model': scopeName + '.' + fieldName
+                    };
+
+                    widget = createWidget(fieldParameters, fieldAttr);
+                }
+                if (widget) {
+                    form.append(widget);
+                }
+            }
+            return form;
+
+        }
+
+    }
+
+    /**
+     * Expose API
      */
     var api = {
+
+        form: function(schema, fields) {
+            return new Form(schema, fields);
+        },
 
     };
     return api;
 
 }]);
+
+/**
+ * Directive for data form
+ */
+EdenMobile.directive("emDataForm", [
+    '$compile', '$emdb', '$emForm',
+    function($compile, $emdb, $emForm) {
+
+        var renderForm = function($scope, elem, attr) {
+
+            var formName = attr.formName;
+
+            $emdb.table(formName).then(function(table) {
+
+                var form = $emForm.form(
+                    table.schema,
+
+                    // @todo: still hardcoded,
+                    //        lookup "_fields" in schema,
+                    //        fall back to all readable fields in schema
+                    ["first_name", "last_name", "date_of_birth"]
+                );
+
+                // Compile the form HTML against the scope,
+                // then render it in place of the directive
+                var compiled = $compile(form.render('form'))($scope);
+                elem.replaceWith(compiled);
+            });
+        };
+
+        return {
+            link: renderForm
+        };
+    }
+]);
